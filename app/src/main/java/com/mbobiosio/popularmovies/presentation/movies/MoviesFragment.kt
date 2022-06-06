bb13dbf0b3f1f42499e0a5f4a14e2ba256289297
mbobiosio <cazewonder@gmail.com>
2022-06-06 15:11:55 +0100

    minor updates

--- app/src/main/java/com/mbobiosio/popularmovies/presentation/movies/MoviesFragment.kt
+++ app/src/main/java/com/mbobiosio/popularmovies/presentation/movies/MoviesFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.mbobiosio.popularmovies.data.remote.model.movie.Movie
 import com.mbobiosio.popularmovies.databinding.FragmentMoviesBinding
 import com.mbobiosio.popularmovies.util.safeNavigate
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 /**
@@ -68,6 +71,15 @@ class MoviesFragment : Fragment() {
                     findNavController().safeNavigate(
                         MoviesFragmentDirections.actionMoviesToDetails(it)
                     )
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            moviesAdapter.loadStateFlow.collectLatest { loadState ->
+                with(binding) {
+                    progressBar.isVisible = loadState.refresh is LoadState.Loading
+                    errorMessage.isVisible = loadState.refresh is LoadState.Error
                 }
             }
         }
